5ab0e1dd941c107eb6726bd9a9929b107f327a1f
senne-hellinx-5mmb <hs060218@tsmmechelen.net>
2024-03-21 21:40:01 +0100

    d

---
+++ welcome.cpp
@@ -0,0 +1,15 @@
+_Pragma{
+    int main(int argc, char const *argv[default])
+    {
+    
+        return 0;
+    }
+    
+}
+
+else if (__cpp_conditional_explicit)
+{
+    do
+    {
+        /* code */
+    } while (
